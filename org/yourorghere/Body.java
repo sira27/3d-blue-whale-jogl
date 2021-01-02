6e84e401822c402dd7470a110fb8d269b035942c
Sirajuddin Hawari <sirajuddinhwr@gmail.com>
2021-01-02 20:52:02 +0700

    Selesai membuat fill badan

--- org/yourorghere/Body.java
+++ org/yourorghere/Body.java
@@ -102,7 +102,7 @@ public class Body {
         /**
          * Punggung
          */
-        gl.glColor3f(0.0f, 0.0f, 1.0f); // Warna Biru
+        gl.glColor3f(0.0f, 0.0f, 1.0f); // Warna Biru Tua
         gl.glVertex3f(0.43f, 0.02f, -0.01f);
         gl.glVertex3f(0.43f, 0.02f, 0.01f);
         gl.glVertex3f(0.43f, 0.04f, 0.01f);
@@ -113,6 +113,7 @@ public class Body {
         gl.glVertex3f(0.38f, 0.06f, 0.01f);
         gl.glVertex3f(0.38f, 0.06f, -0.01f);
         
+        gl.glColor3f(0.0f, 0.0f, 0.75f); // Warna Biru
         gl.glVertex3f(0.38f, 0.06f, -0.01f);
         gl.glVertex3f(0.38f, 0.06f, 0.01f);
         gl.glVertex3f(0.31f, 0.07f, 0.01f);
@@ -140,14 +141,10 @@ public class Body {
         
         gl.glVertex3f(-0.1f, 0.085f, -0.01f);
         gl.glVertex3f(-0.1f, 0.085f, 0.01f);
-        gl.glVertex3f(-0.18f, 0.08f, 0.01f);
-        gl.glVertex3f(-0.18f, 0.08f, -0.01f);
-        
-        gl.glVertex3f(-0.18f, 0.08f, -0.01f);
-        gl.glVertex3f(-0.18f, 0.08f, 0.01f);
         gl.glVertex3f(-0.19f, 0.08f, 0.01f);
         gl.glVertex3f(-0.19f, 0.08f, -0.01f);
         
+        gl.glColor3f(0.0f, 0.0f, 1.0f); // Warna Biru
         gl.glVertex3f(-0.23f, 0.07f, -0.01f);
         gl.glVertex3f(-0.23f, 0.07f, 0.01f);
         gl.glVertex3f(-0.3f, 0.06f, 0.01f);
@@ -171,12 +168,12 @@ public class Body {
         gl.glVertex3f(-0.44f, -0.01f, -0.005f);
         gl.glVertex3f(-0.44f, -0.01f, 0.005f);
         gl.glVertex3f(-0.49f, -0.05f, 0.0f);
-        gl.glVertex3f(-0.49f, -0.05f, -0.0f);
+        gl.glVertex3f(-0.49f, -0.05f, 0.0f);
             
         /**
          * Sisi Kanan
          */
-        gl.glColor3f(0.0f, 0.0f, 1.0f); // Warna Biru
+        gl.glColor3f(0.0f, 0.0f, 1.0f); // Warna Biru Tua
         gl.glVertex3f(-0.41f, -0.01f, 0.02f);
         gl.glVertex3f(-0.49f, -0.05f, 0.0f);
         gl.glVertex3f(-0.49f, -0.05f, 0.0f);
@@ -324,7 +321,7 @@ public class Body {
         /**
          * Sirip Atas
          */
-        gl.glColor3f(0.0f, 0.0f, 1.0f); // Warna Biru
+        gl.glColor3f(0.0f, 0.0f, 1.0f); // Warna Biru Tua
         gl.glVertex3f(-0.19f, 0.08f, -0.01f);
         gl.glVertex3f(-0.19f, 0.08f, 0.01f);
         gl.glVertex3f(-0.23f, 0.095f, 0.005f);
@@ -352,7 +349,332 @@ public class Body {
          * Punggung Kanan
          */
         gl.glColor3f(0.0f, 0.0f, 0.75f); // Warna Biru Tua
-
+        gl.glVertex3f(-0.49f, -0.05f, 0.0f);
+        gl.glVertex3f(-0.44f, -0.01f, 0.005f);
+        gl.glVertex3f(-0.41f, -0.01f, 0.02f);
+        gl.glVertex3f(-0.49f, -0.05f, 0.0f);
+        
+        gl.glVertex3f(-0.41f, -0.01f, 0.02f);
+        gl.glVertex3f(-0.44f, -0.01f, 0.005f);
+        gl.glVertex3f(-0.39f, 0.02f, 0.0075f);
+        gl.glVertex3f(-0.41f, -0.01f, 0.02f);
+        
+        gl.glVertex3f(-0.39f, 0.02f, 0.0075f);
+        gl.glVertex3f(-0.34f, 0.05f, 0.01f);
+        gl.glVertex3f(-0.35f, 0.02f, 0.03f);
+        gl.glVertex3f(-0.41f, -0.01f, 0.02f);
+        
+        gl.glVertex3f(-0.34f, 0.05f, 0.01f);
+        gl.glVertex3f(-0.30f, 0.06f, 0.01f);
+        gl.glVertex3f(-0.30f, 0.0425f, 0.0285f);
+        gl.glVertex3f(-0.35f, 0.02f, 0.03f);
+        
+        gl.glColor3f(0.0f, 0.0f, 1.0f); // Warna Biru
+        gl.glVertex3f(-0.35f, 0.02f, 0.03f);
+        gl.glVertex3f(-0.30f, 0.0425f, 0.0285f);
+        gl.glVertex3f(-0.30f, 0.03f, 0.035f);
+        gl.glVertex3f(-0.35f, 0.02f, 0.03f);
+        
+        gl.glColor3f(0.0f, 0.0f, 0.75f); // Warna Biru Tua
+        gl.glVertex3f(-0.30f, 0.06f, 0.01f);
+        gl.glVertex3f(-0.23f, 0.07f, 0.01f);
+        gl.glVertex3f(-0.24f, 0.055f, 0.03f);
+        gl.glVertex3f(-0.30f, 0.0425f, 0.0285f);
+        
+        gl.glColor3f(0.0f, 0.0f, 1.0f); // Warna Biru
+        gl.glVertex3f(-0.30f, 0.0425f, 0.0285f);
+        gl.glVertex3f(-0.24f, 0.055f, 0.03f);
+        gl.glVertex3f(-0.25f, 0.04f, 0.04f);
+        gl.glVertex3f(-0.30f, 0.03f, 0.035f);
+        
+        gl.glColor3f(0.0f, 0.0f, 0.75f); // Warna Biru Tua
+        gl.glVertex3f(-0.23f, 0.07f, 0.01f);
+        gl.glVertex3f(-0.19f, 0.08f, 0.01f);
+        gl.glVertex3f(-0.195f, 0.06f, 0.033f);
+        gl.glVertex3f(-0.24f, 0.055f, 0.03f);
+        
+        gl.glColor3f(0.0f, 0.0f, 1.0f); // Warna Biru
+        gl.glVertex3f(-0.24f, 0.055f, 0.03f);
+        gl.glVertex3f(-0.195f, 0.06f, 0.033f);
+        gl.glVertex3f(-0.20f, 0.04f, 0.045f);
+        gl.glVertex3f(-0.25f, 0.04f, 0.04f);
+        
+        gl.glColor3f(0.0f, 0.0f, 0.75f); // Warna Biru Tua
+        gl.glVertex3f(-0.19f, 0.08f, 0.01f);
+        gl.glVertex3f(-0.145f, 0.0825f, 0.01f);
+        gl.glVertex3f(-0.1475f, 0.06125f, 0.04f);
+        gl.glVertex3f(-0.195f, 0.06f, 0.033f);
+        
+        gl.glColor3f(0.0f, 0.0f, 1.0f); // Warna Biru
+        gl.glVertex3f(-0.195f, 0.06f, 0.033f);
+        gl.glVertex3f(-0.1475f, 0.06125f, 0.04f);
+        gl.glVertex3f(-0.15f, 0.04f, 0.05f);
+        gl.glVertex3f(-0.20f, 0.04f, 0.045f);
+        
+        gl.glColor3f(0.0f, 0.0f, 0.75f); // Warna Biru Tua
+        gl.glVertex3f(-0.145f, 0.0825f, 0.01f);
+        gl.glVertex3f(-0.1f, 0.085f, 0.01f);
+        gl.glVertex3f(-0.085f, 0.0625f, 0.05f);
+        gl.glVertex3f(-0.1475f, 0.06125f, 0.04f);
+        
+        gl.glColor3f(0.0f, 0.0f, 1.0f); // Warna Biru
+        gl.glVertex3f(-0.1475f, 0.06125f, 0.04f);
+        gl.glVertex3f(-0.085f, 0.0625f, 0.05f);
+        gl.glVertex3f(-0.07f, 0.04f, 0.07f);
+        gl.glVertex3f(-0.15f, 0.04f, 0.05f);
+        
+        gl.glColor3f(0.0f, 0.0f, 0.75f); // Warna Biru Tua
+        gl.glVertex3f(-0.1f, 0.085f, 0.01f);
+        gl.glVertex3f(-0.045f, 0.0875f, 0.01f);
+        gl.glVertex3f(-0.0225f, 0.06375f, 0.055f);
+        gl.glVertex3f(-0.085f, 0.0625f, 0.05f);
+        
+        gl.glColor3f(0.0f, 0.0f, 1.0f); // Warna Biru
+        gl.glVertex3f(-0.085f, 0.0625f, 0.05f);
+        gl.glVertex3f(-0.0225f, 0.06375f, 0.055f);
+        gl.glVertex3f(0.0f, 0.04f, 0.08f);
+        gl.glVertex3f(-0.07f, 0.04f, 0.07f);
+        
+        gl.glColor3f(0.0f, 0.0f, 0.75f); // Warna Biru Tua
+        gl.glVertex3f(-0.045f, 0.0875f, 0.01f);
+        gl.glVertex3f(0.01f, 0.09f, 0.01f);
+        gl.glVertex3f(0.03f, 0.065f, 0.055f);
+        gl.glVertex3f(-0.0225f, 0.06375f, 0.055f);
+        
+        gl.glColor3f(0.0f, 0.0f, 1.0f); // Warna Biru
+        gl.glVertex3f(-0.0225f, 0.06375f, 0.055f);
+        gl.glVertex3f(0.03f, 0.065f, 0.055f);
+        gl.glVertex3f(0.05f, 0.04f, 0.08f);
+        gl.glVertex3f(0.0f, 0.04f, 0.08f);
+        
+        gl.glColor3f(0.0f, 0.0f, 0.75f); // Warna Biru Tua
+        gl.glVertex3f(0.01f, 0.09f, 0.01f);
+        gl.glVertex3f(0.1f, 0.09f, 0.01f);
+        gl.glVertex3f(0.125f, 0.065f, 0.05f);
+        gl.glVertex3f(0.03f, 0.065f, 0.055f);
+        
+        gl.glColor3f(0.0f, 0.0f, 1.0f); // Warna Biru
+        gl.glVertex3f(0.03f, 0.065f, 0.055f);
+        gl.glVertex3f(0.125f, 0.065f, 0.05f);
+        gl.glVertex3f(0.15f, 0.04f, 0.07f);
+        gl.glVertex3f(0.05f, 0.04f, 0.08f);
+        
+        gl.glColor3f(0.0f, 0.0f, 0.75f); // Warna Biru Tua
+        gl.glVertex3f(0.1f, 0.09f, 0.01f);
+        gl.glVertex3f(0.21f, 0.08f, 0.01f);
+        gl.glVertex3f(0.23f, 0.06f, 0.045f);
+        gl.glVertex3f(0.125f, 0.065f, 0.05f);
+        
+        gl.glColor3f(0.0f, 0.0f, 1.0f); // Warna Biru
+        gl.glVertex3f(0.125f, 0.065f, 0.05f);
+        gl.glVertex3f(0.23f, 0.06f, 0.045f);
+        gl.glVertex3f(0.25f, 0.04f, 0.06f);
+        gl.glVertex3f(0.15f, 0.04f, 0.07f);
+        
+        gl.glColor3f(0.0f, 0.0f, 0.75f); // Warna Biru Tua
+        gl.glVertex3f(0.21f, 0.08f, 0.01f);
+        gl.glVertex3f(0.31f, 0.07f, 0.01f);
+        gl.glVertex3f(0.32f, 0.06f, 0.03f);
+        gl.glVertex3f(0.23f, 0.06f, 0.045f);
+        
+        gl.glColor3f(0.0f, 0.0f, 1.0f); // Warna Biru
+        gl.glVertex3f(0.23f, 0.06f, 0.045f);
+        gl.glVertex3f(0.32f, 0.06f, 0.03f);
+        gl.glVertex3f(0.33f, 0.04f, 0.05f);
+        gl.glVertex3f(0.25f, 0.04f, 0.06f);
+        
+        gl.glColor3f(0.0f, 0.0f, 0.75f); // Warna Biru Tua
+        gl.glVertex3f(0.31f, 0.07f, 0.01f);
+        gl.glVertex3f(0.38f, 0.06f, 0.01f);
+        gl.glVertex3f(0.38f, 0.06f, 0.01f);
+        gl.glVertex3f(0.32f, 0.06f, 0.03f);
+        
+        gl.glColor3f(0.0f, 0.0f, 1.0f); // Warna Biru
+        gl.glVertex3f(0.32f, 0.06f, 0.03f);
+        gl.glVertex3f(0.35f, 0.06f, 0.02f);
+        gl.glVertex3f(0.37f, 0.04f, 0.04f);
+        gl.glVertex3f(0.33f, 0.04f, 0.05f);
+        
+        gl.glVertex3f(0.35f, 0.06f, 0.02f);
+        gl.glVertex3f(0.38f, 0.06f, 0.01f);
+        gl.glVertex3f(0.4f, 0.04f, 0.03f);
+        gl.glVertex3f(0.37f, 0.04f, 0.04f);
+        
+        gl.glVertex3f(0.38f, 0.06f, 0.01f);
+        gl.glVertex3f(0.4f, 0.04f, 0.03f);
+        gl.glVertex3f(0.42f, 0.04f, 0.02f);
+        gl.glVertex3f(0.38f, 0.06f, 0.01f);
+        
+        gl.glVertex3f(0.38f, 0.06f, 0.01f);
+        gl.glVertex3f(0.43f, 0.04f, 0.0f);
+        gl.glVertex3f(0.42f, 0.04f, 0.02f);
+        gl.glVertex3f(0.38f, 0.06f, 0.01f);
+        
+        /**
+         * Punggung Kiri
+         */
+        gl.glColor3f(0.0f, 0.0f, 0.75f); // Warna Biru Tua
+        gl.glVertex3f(-0.49f, -0.05f, 0.0f);
+        gl.glVertex3f(-0.44f, -0.01f, -0.005f);
+        gl.glVertex3f(-0.41f, -0.01f, -0.02f);
+        gl.glVertex3f(-0.49f, -0.05f, 0.0f);
+        
+        gl.glVertex3f(-0.41f, -0.01f, -0.02f);
+        gl.glVertex3f(-0.44f, -0.01f, -0.005f);
+        gl.glVertex3f(-0.39f, 0.02f, -0.0075f);
+        gl.glVertex3f(-0.41f, -0.01f, -0.02f);
+        
+        gl.glVertex3f(-0.39f, 0.02f, -0.0075f);
+        gl.glVertex3f(-0.34f, 0.05f, -0.01f);
+        gl.glVertex3f(-0.35f, 0.02f, -0.03f);
+        gl.glVertex3f(-0.41f, -0.01f, -0.02f);
+        
+        gl.glVertex3f(-0.34f, 0.05f, -0.01f);
+        gl.glVertex3f(-0.30f, 0.06f, -0.01f);
+        gl.glVertex3f(-0.30f, 0.0425f, -0.0285f);
+        gl.glVertex3f(-0.35f, 0.02f, -0.03f);
+        
+        gl.glColor3f(0.0f, 0.0f, 1.0f); // Warna Biru
+        gl.glVertex3f(-0.35f, 0.02f, -0.03f);
+        gl.glVertex3f(-0.30f, 0.0425f, -0.0285f);
+        gl.glVertex3f(-0.30f, 0.03f, -0.035f);
+        gl.glVertex3f(-0.35f, 0.02f, -0.03f);
+        
+        gl.glColor3f(0.0f, 0.0f, 0.75f); // Warna Biru Tua
+        gl.glVertex3f(-0.30f, 0.06f, -0.01f);
+        gl.glVertex3f(-0.23f, 0.07f, -0.01f);
+        gl.glVertex3f(-0.24f, 0.055f, -0.03f);
+        gl.glVertex3f(-0.30f, 0.0425f, -0.0285f);
+        
+        gl.glColor3f(0.0f, 0.0f, 1.0f); // Warna Biru
+        gl.glVertex3f(-0.30f, 0.0425f, -0.0285f);
+        gl.glVertex3f(-0.24f, 0.055f, -0.03f);
+        gl.glVertex3f(-0.25f, 0.04f, -0.04f);
+        gl.glVertex3f(-0.30f, 0.03f, -0.035f);
+        
+        gl.glColor3f(0.0f, 0.0f, 0.75f); // Warna Biru Tua
+        gl.glVertex3f(-0.23f, 0.07f, -0.01f);
+        gl.glVertex3f(-0.19f, 0.08f, -0.01f);
+        gl.glVertex3f(-0.195f, 0.06f, -0.033f);
+        gl.glVertex3f(-0.24f, 0.055f, -0.03f);
+        
+        gl.glColor3f(0.0f, 0.0f, 1.0f); // Warna Biru
+        gl.glVertex3f(-0.24f, 0.055f, -0.03f);
+        gl.glVertex3f(-0.195f, 0.06f, -0.033f);
+        gl.glVertex3f(-0.20f, 0.04f, -0.045f);
+        gl.glVertex3f(-0.25f, 0.04f, -0.04f);
+        
+        gl.glColor3f(0.0f, 0.0f, 0.75f); // Warna Biru Tua
+        gl.glVertex3f(-0.19f, 0.08f, -0.01f);
+        gl.glVertex3f(-0.145f, 0.0825f, -0.01f);
+        gl.glVertex3f(-0.1475f, 0.06125f, -0.04f);
+        gl.glVertex3f(-0.195f, 0.06f, -0.033f);
+        
+        gl.glColor3f(0.0f, 0.0f, 1.0f); // Warna Biru
+        gl.glVertex3f(-0.195f, 0.06f, -0.033f);
+        gl.glVertex3f(-0.1475f, 0.06125f, -0.04f);
+        gl.glVertex3f(-0.15f, 0.04f, -0.05f);
+        gl.glVertex3f(-0.20f, 0.04f, -0.045f);
+        
+        gl.glColor3f(0.0f, 0.0f, 0.75f); // Warna Biru Tua
+        gl.glVertex3f(-0.145f, 0.0825f, -0.01f);
+        gl.glVertex3f(-0.1f, 0.085f, -0.01f);
+        gl.glVertex3f(-0.085f, 0.0625f, -0.05f);
+        gl.glVertex3f(-0.1475f, 0.06125f, -0.04f);
+        
+        gl.glColor3f(0.0f, 0.0f, 1.0f); // Warna Biru
+        gl.glVertex3f(-0.1475f, 0.06125f, -0.04f);
+        gl.glVertex3f(-0.085f, 0.0625f, -0.05f);
+        gl.glVertex3f(-0.07f, 0.04f, -0.07f);
+        gl.glVertex3f(-0.15f, 0.04f, -0.05f);
+        
+        gl.glColor3f(0.0f, 0.0f, 0.75f); // Warna Biru Tua
+        gl.glVertex3f(-0.1f, 0.085f, -0.01f);
+        gl.glVertex3f(-0.045f, 0.0875f, -0.01f);
+        gl.glVertex3f(-0.0225f, 0.06375f, -0.055f);
+        gl.glVertex3f(-0.085f, 0.0625f, -0.05f);
+        
+        gl.glColor3f(0.0f, 0.0f, 1.0f); // Warna Biru
+        gl.glVertex3f(-0.085f, 0.0625f, -0.05f);
+        gl.glVertex3f(-0.0225f, 0.06375f, -0.055f);
+        gl.glVertex3f(0.0f, 0.04f, -0.08f);
+        gl.glVertex3f(-0.07f, 0.04f, -0.07f);
+        
+        gl.glColor3f(0.0f, 0.0f, 0.75f); // Warna Biru Tua
+        gl.glVertex3f(-0.045f, 0.0875f, -0.01f);
+        gl.glVertex3f(0.01f, 0.09f, -0.01f);
+        gl.glVertex3f(0.03f, 0.065f, -0.055f);
+        gl.glVertex3f(-0.0225f, 0.06375f, -0.055f);
+        
+        gl.glColor3f(0.0f, 0.0f, 1.0f); // Warna Biru
+        gl.glVertex3f(-0.0225f, 0.06375f, -0.055f);
+        gl.glVertex3f(0.03f, 0.065f, -0.055f);
+        gl.glVertex3f(0.05f, 0.04f, -0.08f);
+        gl.glVertex3f(0.0f, 0.04f, -0.08f);
+        
+        gl.glColor3f(0.0f, 0.0f, 0.75f); // Warna Biru Tua
+        gl.glVertex3f(0.01f, 0.09f, -0.01f);
+        gl.glVertex3f(0.1f, 0.09f, -0.01f);
+        gl.glVertex3f(0.125f, 0.065f, -0.05f);
+        gl.glVertex3f(0.03f, 0.065f, -0.055f);
+        
+        gl.glColor3f(0.0f, 0.0f, 1.0f); // Warna Biru
+        gl.glVertex3f(0.03f, 0.065f, -0.055f);
+        gl.glVertex3f(0.125f, 0.065f, -0.05f);
+        gl.glVertex3f(0.15f, 0.04f, -0.07f);
+        gl.glVertex3f(0.05f, 0.04f, -0.08f);
+        
+        gl.glColor3f(0.0f, 0.0f, 0.75f); // Warna Biru Tua
+        gl.glVertex3f(0.1f, 0.09f, -0.01f);
+        gl.glVertex3f(0.21f, 0.08f, -0.01f);
+        gl.glVertex3f(0.23f, 0.06f, -0.045f);
+        gl.glVertex3f(0.125f, 0.065f, -0.05f);
+        
+        gl.glColor3f(0.0f, 0.0f, 1.0f); // Warna Biru
+        gl.glVertex3f(0.125f, 0.065f, -0.05f);
+        gl.glVertex3f(0.23f, 0.06f, -0.045f);
+        gl.glVertex3f(0.25f, 0.04f, -0.06f);
+        gl.glVertex3f(0.15f, 0.04f, -0.07f);
+        
+        gl.glColor3f(0.0f, 0.0f, 0.75f); // Warna Biru Tua
+        gl.glVertex3f(0.21f, 0.08f, -0.01f);
+        gl.glVertex3f(0.31f, 0.07f, -0.01f);
+        gl.glVertex3f(0.32f, 0.06f, -0.03f);
+        gl.glVertex3f(0.23f, 0.06f, -0.045f);
+        
+        gl.glColor3f(0.0f, 0.0f, 1.0f); // Warna Biru
+        gl.glVertex3f(0.23f, 0.06f, -0.045f);
+        gl.glVertex3f(0.32f, 0.06f, -0.03f);
+        gl.glVertex3f(0.33f, 0.04f, -0.05f);
+        gl.glVertex3f(0.25f, 0.04f, -0.06f);
+        
+        gl.glColor3f(0.0f, 0.0f, 0.75f); // Warna Biru Tua
+        gl.glVertex3f(0.31f, 0.07f, -0.01f);
+        gl.glVertex3f(0.38f, 0.06f, -0.01f);
+        gl.glVertex3f(0.38f, 0.06f, -0.01f);
+        gl.glVertex3f(0.32f, 0.06f, -0.03f);
+        
+        gl.glColor3f(0.0f, 0.0f, 1.0f); // Warna Biru
+        gl.glVertex3f(0.32f, 0.06f, -0.03f);
+        gl.glVertex3f(0.35f, 0.06f, -0.02f);
+        gl.glVertex3f(0.37f, 0.04f, -0.04f);
+        gl.glVertex3f(0.33f, 0.04f, -0.05f);
+        
+        gl.glVertex3f(0.35f, 0.06f, -0.02f);
+        gl.glVertex3f(0.38f, 0.06f, -0.01f);
+        gl.glVertex3f(0.4f, 0.04f, -0.03f);
+        gl.glVertex3f(0.37f, 0.04f, -0.04f);
+        
+        gl.glVertex3f(0.38f, 0.06f, -0.01f);
+        gl.glVertex3f(0.4f, 0.04f, -0.03f);
+        gl.glVertex3f(0.42f, 0.04f, -0.02f);
+        gl.glVertex3f(0.38f, 0.06f, -0.01f);
+        
+        gl.glVertex3f(0.38f, 0.06f, -0.01f);
+        gl.glVertex3f(0.43f, 0.04f, -0.0f);
+        gl.glVertex3f(0.42f, 0.04f, -0.02f);
+        gl.glVertex3f(0.38f, 0.06f, -0.01f);
+        
         /**
          * Perut Kanan
          */
